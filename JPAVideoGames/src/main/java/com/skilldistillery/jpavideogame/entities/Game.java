bb7d94cc8d35479d6fac487c933f98db4e732592
lisies93 <solorzanolisseth83@gmail.com>
2021-03-12 17:11:08 -0700

    Stub and test JPA/Hibernate'

---
+++ JPAVideoGames/src/main/java/com/skilldistillery/jpavideogame/entities/Game.java
@@ -0,0 +1,41 @@
+package com.skilldistillery.jpavideogame.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Game {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private int id;
+private String name;
+	
+public Game() {}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getName() {
+	return name;
+}
+
+public void setName(String name) {
+	this.name = name;
+}
+
+@Override
+public String toString() {
+	return "Game [id=" + id + ", name=" + name + "]";
+}
+
+
+
+}
